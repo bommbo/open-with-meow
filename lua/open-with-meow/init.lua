67d3f6b3fcee341e522e72626ad41261ea575f0e
bommbo <61496351+bommbo@users.noreply.github.com>
2025-04-03 00:42:46 +0800

    Update init.lua

--- lua/open-with-meow/init.lua
+++ lua/open-with-meow/init.lua
@@ -4,7 +4,7 @@ local function launch()
 	local file_names = vim.api.nvim_buf_get_name(current_buf)
 	-- print("Current File:", file_names)
 	local cursor = vim.api.nvim_win_get_cursor(0) -- (row, col)
-	local cursor_args = string.format("+%d:%d", cursor[1], cursor[2] + 2)
+	local cursor_args = string.format("+%d:%d", cursor[1], cursor[2] + 1)
 	-- print(string.format("Cursor Arg: %s", cursor_args))
 
 	-- eg: file: a.txt / line number -> 1 / column number -> 2
