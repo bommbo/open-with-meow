f8484b99f87ff20eacb2cc6849904438fcd13c78
bommbo <61496351+bommbo@users.noreply.github.com>
2025-04-03 00:33:03 +0800

    Update init.lua

--- lua/open-with-meow/init.lua
+++ lua/open-with-meow/init.lua
@@ -1,24 +1,15 @@
 local function launch()
-	local buffers = vim.api.nvim_list_bufs()
 
-	local cursor_args = {}
-	for _, buffer in ipairs(buffers) do
-		if vim.api.nvim_buf_is_valid(buffer) then
-			local cursor = vim.api.nvim_win_get_cursor(0)
-			table.insert(cursor_args, "+" .. cursor[1] .. ":" .. cursor[2] + 1)
-		end
-	end
+	local current_buf = vim.api.nvim_get_current_buf()
+	local file_names = vim.api.nvim_buf_get_name(current_buf)
+	-- print("Current File:", file_names)
+	local cursor = vim.api.nvim_win_get_cursor(0) -- (row, col)
+	local cursor_args = string.format("+%d:%d", cursor[1], cursor[2] + 2)
+	-- print(string.format("Cursor Arg: %s", cursor_args))
 
-	local file_names = {}
-	for _, buffer in ipairs(buffers) do
-		if vim.api.nvim_buf_is_valid(buffer) then
-			table.insert(file_names, vim.api.nvim_buf_get_name(buffer))
-		end
-	end
-	-- commandline: emacs +line numbers:column numbers file_name
 	-- eg: file: a.txt / line number -> 1 / column number -> 2
 	-- emacs +1:2 a.txt
-	vim.cmd(string.format("silent !alacritty -e emacs -nw %s %s &", table.concat(cursor_args, " "), table.concat(file_names, " ")))
+	vim.cmd(string.format("silent !alacritty -e emacs -nw %s %s &", cursor_args, file_names))
 end
 return {
 	launch = launch,
